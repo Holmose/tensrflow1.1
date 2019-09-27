b4cffded2e3e2653b7328194c2312b59af4e192b
holmose <1310727009@qq.com>
2019-09-27 11:45:31 +0800

    搭建环境

--- tensor1.py
+++ tensor1.py
@@ -1,7 +1,7 @@
 ﻿#python35 tensorflow==1.1
 
 import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"]=''
+os.environ["TF_CPP_MIN_LOG_LEVEL"]='3'
 
 
 import tensorflow as tf
@@ -15,8 +15,18 @@ input1 = tf.placeholder(tf.float32)
 input2 = tf.placeholder(tf.float32)
 output = tf.multiply(input1, input2)
 
+a = tf.constant([[-1.0, 2.0, 3.0, 4.0]])
+
+
 product = tf.matmul(matrix1, matrix2)
 
 with tf.Session() as sess:
     result = sess.run([product, output], feed_dict={input1:[7.], input2:[2.]})
-    print(result)
+    print('result', result)
+
+    b = tf.nn.dropout(a, 0.5, noise_shape = [1, 4])
+    print('b noise_shape [1, 4]', sess.run(b))
+
+    b = tf.nn.dropout(a, 0.5, noise_shape = [1, 1])
+    print('b noise_shape [1, 1]', sess.run(b))
+
